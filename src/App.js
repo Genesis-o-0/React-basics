93bccd856251d2511965403aa0ffe5e0e2969e32
Genesis-o-0 <mohamed.ahmed.2912023@gmail.com>
2023-10-23 01:15:03 +0300

    Add conditional styling and rendering

--- src/App.js
+++ src/App.js
@@ -2,35 +2,18 @@ import "./App.css";
 
 // App Component
 function App() {
-  console.log(<User />);
-  /**
-   * so <User /> is a Component and an Object with these properties ($$typeof, key, props(this is an Object also), ref, type...etc)
-   *
-   * Let's focus on props
-   * ///////////////////
-   * in React documentations it says that "React components use props to communicate with each other. Every parent component can pass some information to its child components by giving them props. Props might remind you of HTML attributes, but you can pass any JavaScript value through them, including objects, arrays, and functions."
-   *
-   * based on that we can use props to pass parameters-like properties to the component and then use them in rendering
-   */
+  // Conditional rendering
+  const age = 18;
+  const isGreen = true;
+
   return (
     <div className="App">
-      {/* passing props to the User component */}
-      <User name="mohamed" age="26" email="example@example.com" />
-      <User name="Sarah" age="30" email="example2@example.com" />
+      {/* Using Ternary operator for conditional rendering */}
+      {age >= 18 ? <h1>Over age</h1> : <h1>Under age</h1>}
+      {/* Using Ternary operator for conditional Styling */}
+      <h1 style={{ color: isGreen ? "green" : "red" }}>This has a color</h1>
     </div>
   );
 }
-
-// User Component
-const User = (props) => {
-  return (
-    <div>
-      <h1>{props.name}</h1>
-      <h1>{props.age}</h1>
-      <h1>{props.email}</h1>
-    </div>
-  );
-};
-console.log(<User />);
 
 export default App;
