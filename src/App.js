3cfbe801e0947fd8d663df6652279578e9e72023
Genesis-o-0 <mohamed.ahmed.2912023@gmail.com>
2023-10-23 02:49:23 +0300

    Add working without state

--- src/App.js
+++ src/App.js
@@ -1,36 +1,23 @@
 import "./App.css";
 
-// App Component
 function App() {
-  console.log(<User />);
-  /**
-   * so <User /> is a Component and an Object with these properties ($$typeof, key, props(this is an Object also), ref, type...etc)
-   *
-   * Let's focus on props
-   * ///////////////////
-   * in React documentations it says that "React components use props to communicate with each other. Every parent component can pass some information to its child components by giving them props. Props might remind you of HTML attributes, but you can pass any JavaScript value through them, including objects, arrays, and functions."
-   *
-   * based on that we can use props to pass parameters-like properties to the component and then use them in rendering
-   */
+  // Working without states
+  let age = 0;
+  // this will update the value of age in the console but not in the document
+  // react will render this component once at the beginning where age is equal to 0
+  // but at the same time react has no clue about the "state" of the age which is increasing in the console!
+  // so we need a way to tell react to watch 'current state' of the age variable and update the document based on it
+  const increaseAge = () => {
+    age += 1;
+    console.log(age);
+  };
   return (
     <div className="App">
-      {/* passing props to the User component */}
-      <User name="mohamed" age="26" email="example@example.com" />
-      <User name="Sarah" age="30" email="example2@example.com" />
+      {age}
+      <br />
+      <button onClick={increaseAge}>Increase age</button>
     </div>
   );
 }
-
-// User Component
-const User = (props) => {
-  return (
-    <div>
-      <h1>{props.name}</h1>
-      <h1>{props.age}</h1>
-      <h1>{props.email}</h1>
-    </div>
-  );
-};
-console.log(<User />);
 
 export default App;
