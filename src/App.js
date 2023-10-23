1ee281e53d824da910a8b0cbe1d045acd7906e34
Genesis-o-0 <mohamed.ahmed.2912023@gmail.com>
2023-10-23 12:25:22 +0300

    Add show-hide with useState

--- src/App.js
+++ src/App.js
@@ -2,17 +2,15 @@ import "./App.css";
 import { useState } from "react";
 function App() {
   // Working with states
-  const [age, setAge] = useState(0);
-  // working with useState Hook to tell react to watch over the age variable and trigger a re-render whenever it is changed
-  // useState is a Hook which returns an array the contains the current state and the set function that lets you update the state to a different value and trigger a re-render.
-  const increaseAge = () => {
-    setAge(age + 1);
+  const [show, setShow] = useState(true);
+
+  const handleShow = (show) => {
+    setShow(!show);
   };
   return (
     <div className="App">
-      {age}
-      <br />
-      <button onClick={increaseAge}>Increase age</button>
+      {show ? <h1>Shown</h1> : <h1>Hidden</h1>}
+      <button onClick={() => handleShow(show)}>{show ? "Hide" : "Show"}</button>
     </div>
   );
 }
