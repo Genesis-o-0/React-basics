675aebc8cb029ffb62e23779377021d0b992fc6b
Genesis-o-0 <mohamed.ahmed.2912023@gmail.com>
2023-10-23 22:53:33 +0300

    Add Component lifecycle and useEffect Hook

--- src/App.js
+++ src/App.js
@@ -1,42 +1,24 @@
 import "./App.css";
 import { useState } from "react";
-import Task from "./Task";
+import Text from "./Text";
 
 function App() {
-  const [todoList, setTodoList] = useState([]);
-  const [newTask, setNewTask] = useState("");
+ 
+  const [showText, setShowText] = useState(false);
 
-  const handleChange = (e) => {
-    if (e.target.value.trim()) {
-      setNewTask(e.target.value);
-    } else {
-      setNewTask("");
-    }
-  };
 
-  const addTask = () => {
-    if (newTask) {
-      setTodoList([...todoList, newTask]);
-    }
-  };
-
-  const deleteTask = (index) => {
-    const updatedTodoList = [...todoList];
-    updatedTodoList.splice(index, 1);
-    setTodoList(updatedTodoList);
-  };
 
   return (
     <div className="App">
-      <div className="addTask">
-        <input type="text" onChange={handleChange} />
-        <button onClick={addTask}>Add Task</button>
-      </div>
-      <div className="list">
-        {todoList.map((task, index) => (
-          <Task deleteTask={deleteTask} taskName={task} key={index} i={index} />
-        ))}
-      </div>
+      <button
+        onClick={() => {
+          setShowText(!showText);
+        }}
+      >
+        Show Text
+      </button>
+
+      {showText && <Text />}
     </div>
   );
 }
